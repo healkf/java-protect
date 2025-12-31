b63a38a953163d2692b24007fb207cf46a29539d
healkf <pumba.pumbakk@gmail.com>
2025-12-31 12:04:59 +0300

    добавлен первый патч защиты (Launcher, ProtectorTool, EncryptedClassLoader, StringObf)

---
+++ src/main/java/org/putika/ProtectorTool.java
@@ -0,0 +1,17 @@
+package org.putika;
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.jar.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/putika/StringObf.java
@@ -0,0 +1,2 @@
+package org.putika;public class StringObf {
+}
